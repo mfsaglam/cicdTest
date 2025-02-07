3f9a284f4c564b0366139c091c8f84faa528ce69
mfsaglam <mfsaglam1@gmail.com>
2025-02-07 23:30:38 +0300

    Initial Commit

---
+++ cicdTestApp/cicdTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  cicdTestAppApp.swift
+//  cicdTestApp
+//
+//  Created by Fatih Sağlam on 7.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct cicdTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
